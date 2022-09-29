2ef94b9919b97604fbe7a8099f2b1f9b6fd9c97c
Timothy Downing <downingkeys@gmail.com>
2022-09-29 16:02:10 -0700

    Finish working and refactoring palindrome method

--- lib/tdowning_palindrome.rb
+++ lib/tdowning_palindrome.rb
@@ -1,8 +1,16 @@
-# frozen_string_literal: true
-
 require_relative "tdowning_palindrome/version"
 
-module TdowningPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,5 @@
-# frozen_string_literal: true
-
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "tdowning_palindrome"
-
 require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use!
